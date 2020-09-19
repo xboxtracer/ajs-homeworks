3f2983cbcd70b6c79d979f0b8783bbda33134700
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-09-19 23:03:59 +0300

    Настроил подключение CSS и ещё одного JS из их папок

--- modules/webpacktest/src/index.js
+++ modules/webpacktest/src/index.js
@@ -1,1 +1,4 @@
+import './css/style.css';
+import './js/app.js';
+
 console.log('Hellou Uorld');
--- modules/webpacktest/webpack.config.js
+++ modules/webpacktest/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -8,13 +10,18 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "babel-loader"
+            }
         }
-      }
     ]
-  }
+  },
+  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()]
 }
