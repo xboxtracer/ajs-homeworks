dc916ade758c5cfceeb1827ee379a69465d48d71
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-10-20 01:03:40 +0300

    Дописал Set и Map классы

--- prototypes-constructors/src/js/__tests__/container.test.js
+++ prototypes-constructors/src/js/__tests__/container.test.js
@@ -1,9 +1,48 @@
-import { Validator, changePhoneNumber } from '../regExp';
+import { Team, ErrorRepository } from '../container';
 
-test('check Validator for correct', () => {
-  const valid = new Validator('Nik111ta');
-  const result = valid.validateUsername();
-  const expected = true;
+test('check setContainer size after .add() and .addAll() ', () => {
+  const expected = 4
+  const setContainer = new Team()
+  setContainer.add(6)
+  setContainer.addAll(6, 7, 8, 9)
+  setContainer.addAll(8, 9)
+  setContainer.addAll(6, 7, 8)
+  const result = setContainer.member.size
+  expect(result).toBe(expected);
+});
+
+test('check setContainerator for correct', () => {  
+  const setContainer = new Team()
+  setContainer.addAll(6, 7, 8, 9)
+
+  const expected = [6, 7, 8, 9]
+  const result = setContainer.toArray()
+
+  expect(result).toEqual(expected);
+});
+
+test('check ErrorRepository for correct error', () => { 
+  const errMap = new ErrorRepository();
+  const newError = {
+      value: 'someError',
+      code: 992
+  }
+  errMap.setErr(newError)
+  const result = errMap.translate(992)
+  const expected = 'someError'
+
+  expect(result).toBe(expected);
+});
+
+test('check ErrorRepository for unknown error', () => { 
+  const errMap = new ErrorRepository();
+  const newError = {
+      value: 'someError',
+      code: 992
+  }
+  errMap.setErr(newError)
+  const result = errMap.translate(995)
+  const expected = 'Unknown error'
 
   expect(result).toBe(expected);
 });
--- prototypes-constructors/src/js/container.js
+++ prototypes-constructors/src/js/container.js
@@ -1,5 +1,4 @@
-
-class Team {
+export class Team {
     constructor() {
        this.member = new Set()
     }
@@ -14,4 +13,23 @@ class Team {
       this.member.forEach(val => arr.push(val))
       return arr
     }
+}
+
+
+export class ErrorRepository {
+  constructor() {
+    this.err = new Map()
+  }
+  setErr(err) {
+    this.err.set(err.code, err.value);
+  }
+  translate(code) {
+    let a
+    if (this.err.has(code)) {
+      a = this.err.get(code);
+    } else {
+      a = 'Unknown error' 
+    }
+    return a
+  }
 }
